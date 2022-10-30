de6a56399de380fbaf0f521f458b7cc88b375a09
truedjentleman <andrey.burluckiy@gmail.com>
2022-10-30 12:12:12 +0100

    types definitions updated for SelectOption

--- src/Select.tsx
+++ src/Select.tsx
@@ -2,7 +2,7 @@ import styles from './select.module.css'
 
 type SelectOption = {
   label: string
-  value: string | number
+  value: any
 }
 
 type SelectProps = {
@@ -14,7 +14,7 @@ type SelectProps = {
 export function Select({ value, options, onChange }: SelectProps) {
   return (
     <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>Value</span>
+      <span className={styles.value}>{value.label}</span>
       <button className={styles['clear-btn']}>&times;</button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
